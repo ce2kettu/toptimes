6debbd65658727a7fb86dc286610339158363c12
Teemu Ketonen <teemu.ket@gmail.com>
2019-04-13 16:11:10 +0300

    Update leaderboard_s.lua

--- leaderboard_s.lua
+++ leaderboard_s.lua
@@ -9,7 +9,7 @@ Leaderboard.selectQuery = [[
 	INNER JOIN ddc_accounts AS accounts ON leaderboard.player_id = accounts.id
 	INNER JOIN ddc_maps AS maps ON leaderboard.map_id = maps.id
 	WHERE maps.map_resource = ?
-	ORDER BY time_ms
+	ORDER BY time_ms, recorded_at
 	LIMIT ?;
 ]]
 
@@ -37,9 +37,7 @@ Leaderboard.personalBestQuery = [[
 	WHERE accounts.player_account IN (?);
 ]]
 
-
 function Leaderboard.updatePlayerCountry()
-
 	local account = getElementData(source, "account")
 	
 	if not account then return end	
@@ -53,14 +51,11 @@ function Leaderboard.updatePlayerCountry()
 	if not connection then return end	
 	
 	dbExec(connection, "UPDATE ddc_accounts SET player_country = ? WHERE player_account = ?;", country, account)
-	
 end
 addEvent("onPlayerLoggedIn", true)
 addEventHandler("onPlayerLoggedIn", root, Leaderboard.updatePlayerCountry)
 
-
 function Leaderboard.addRecord(player, time)
-
 	local arenaElement = getElementParent(player)
 	local map = getElementData(arenaElement, "map")
 	local account = getElementData(player, "account")
@@ -72,21 +67,15 @@ function Leaderboard.addRecord(player, time)
 	outputChatBox("#FF5050You finished at "..exports["CCS"]:export_msToTime(time, true), player, 255, 255, 255, true)
 
 	if not getElementData(arenaElement, "toptimes") then
-	
 		return outputChatBox("#FF5050This Arena does not have top times enabled", player, 255, 255, 255, true)
-	
 	end
 
 	if not account then
-	
 		return outputChatBox("#FF5050Guests cannot get top times", player, 255, 255, 255, true)
-	
 	end
 	
 	if getElementData(player, "setting:ban_toptimes") == 1 then
-	
 		return outputChatBox("#FF5050You are banned from getting top times", player, 255, 255, 255, true)
-	
 	end
 
 	local connection = exports["CCS_database"]:getConnection()
@@ -97,13 +86,10 @@ function Leaderboard.addRecord(player, time)
 	dbExec(connection, query)
 
 	local query2 = dbPrepareString(connection, Leaderboard.selectQuery, map.resource, Leaderboard.ROWS_TO_SHOW)
-	dbQuery(Leaderboard.callback, {arenaElement, false, account}, connection, query2)
-	
+	dbQuery(Leaderboard.callback, {arenaElement, false, account, player, true}, connection, query2)
 end
 
-
-function Leaderboard.callback(handle, arenaElement, isInitialFetch, account)
-
+function Leaderboard.callback(handle, arenaElement, isInitialFetch, account, player, isUpdateRequired)
 	local result = dbPoll(handle, 0)
 	local map = getElementData(arenaElement, "map")
 
@@ -140,17 +126,14 @@ function Leaderboard.callback(handle, arenaElement, isInitialFetch, account)
 	if not Leaderboard.oldCache[arenaElement] then return end
 	
 	for i = 1, Leaderboard.ROWS_TO_SHOW, 1 do
-		
-		if not Leaderboard.oldCache[arenaElement] and not result[i] then return end
+		if not Leaderboard.oldCache[arenaElement][i] and result[i] then return end
 
 		--someone deleted a top time
 		if Leaderboard.oldCache[arenaElement][i] and not result[i] then
-		
 			Leaderboard.fetchAllPersonalBestTimes(arenaElement)
 
 		--someone got a new top without replacing the old one
 		elseif result[i] and not Leaderboard.oldCache[arenaElement][i] then
-		
 			if account and result[i].player_account == account then
 				if i == 1 then
 					exports["CCS"]:export_outputGlobalChat("#FF0000* #FFFFFF"..result[i].player_name:gsub("#%x%x%x%x%x%x", "").." got a new #"..i.." top time on "..map.name)
@@ -163,29 +146,25 @@ function Leaderboard.callback(handle, arenaElement, isInitialFetch, account)
 
 		--someone got a new top time
 		elseif Leaderboard.oldCache[arenaElement][i].time ~= result[i].time_ms then
-
 			if account and result[i].player_account == account then
 				if i == 1 then
-					exports["CCS"]:export_outputGlobalChat("#ffffff"..result[i].player_name:gsub("#%x%x%x%x%x%x", "").." #ff5050got a new #ffffff#"..i.." #ff5050top time on #ffffff"..map.name.."#ff5050!")
+					exports["CCS"]:export_outputGlobalChat("#FF0000* #FFFFFF"..result[i].player_name:gsub("#%x%x%x%x%x%x", "").." got a new #"..i.." top time on "..map.name)
 				else
 					exports["CCS"]:export_outputArenaChat(arenaElement, "#FF0000* #FFFFFF"..result[i].player_name:gsub("#%x%x%x%x%x%x", "").." got a new #"..i.." top time")
 				end
 			end
 
 			Leaderboard.fetchAllPersonalBestTimes(arenaElement)
-
-		else 
-
-			Leaderboard.fetchPersonalBestTime()
-
+		elseif isElement(player) and isUpdateRequired then
+			Leaderboard.fetchPersonalBestTime(player)
 		end
 	end
 end
 
+function Leaderboard.fetchPersonalBestTime(requestedPlayer)
+	if not isElement(requestedPlayer) then return end
 
-function Leaderboard.fetchPersonalBestTime()
-
-	local arenaElement = getElementParent(source)
+	local arenaElement = getElementParent(requestedPlayer)
 	local map = getElementData(arenaElement, "map")
 
 	if not map then return end
@@ -194,28 +173,25 @@ function Leaderboard.fetchPersonalBestTime()
 
 	if not connection then return end
 
-	local account = getElementData(source, "account")
+	local account = getElementData(requestedPlayer, "account")
 	local players = {}
-	table.insert(players, source)
+	table.insert(players, requestedPlayer)
 
 	if account then
-		
 		local query = dbPrepareString(connection, Leaderboard.personalBestQuery, map.resource, account)
 		dbQuery(Leaderboard.personalBestCallback, {arenaElement, players}, connection, query)
-
 	else
-
 		Leaderboard.personalBestCallback(nil, arenaElement, players, true)
-
 	end
+end
 
+function Leaderboard.arenaJoin()
+	Leaderboard.fetchPersonalBestTime(source)
 end
 addEvent("onPlayerJoinArena", true)
-addEventHandler("onPlayerJoinArena", root, Leaderboard.fetchPersonalBestTime)
-
+addEventHandler("onPlayerJoinArena", root, Leaderboard.arenaJoin)
 
 function Leaderboard.fetchAllPersonalBestTimes(arenaElement)
-
 	local map = getElementData(arenaElement, "map")
 
 	if not map then return end
@@ -228,17 +204,14 @@ function Leaderboard.fetchAllPersonalBestTimes(arenaElement)
 	local accountsToFetch = {}
 
 	for i, player in ipairs(players) do
-	
 		local account = getElementData(player, "account")
 
 		if account then
 			table.insert(accountsToFetch, "'"..account.."'")
-		end
-		
+		end	
 	end
 
 	if #accountsToFetch ~= 0 then
-		
 		local values = table.concat(accountsToFetch, ",")
 		local query = [[
 			SELECT rank, accounts.player_account, accounts.player_name, accounts.player_country, time_ms, recorded_at 
@@ -253,18 +226,12 @@ function Leaderboard.fetchAllPersonalBestTimes(arenaElement)
 
 		local query2 = dbPrepareString(connection, query, map.resource, values)
 		dbQuery(Leaderboard.personalBestCallback, {arenaElement, players}, connection, query2)
-		
 	else
-
 		Leaderboard.personalBestCallback(nil, arenaElement, players, true)
-
 	end
-
 end
 
-
 function Leaderboard.personalBestCallback(handle, arenaElement, players, isManual)
-
 	local map = getElementData(arenaElement, "map")
 
 	if not isManual then
@@ -273,43 +240,31 @@ function Leaderboard.personalBestCallback(handle, arenaElement, players, isManua
 		if not result then
 			dbFree(handle)
 		else
-	end
-
-		Leaderboard.personalBestCache[arenaElement] = {}
-
-		for i, row in ipairs(result) do
-			Leaderboard.personalBestCache[arenaElement][row.player_account] = {
-				rank = row.rank,
-				account = row.player_account,
-				nickname = row.player_name,
-				country = row.player_country,
-				time = row.time_ms,
-				date = row.recorded_at
-			}
+			Leaderboard.personalBestCache[arenaElement] = {}
+		
+			for i, row in ipairs(result) do
+				Leaderboard.personalBestCache[arenaElement][row.player_account] = {
+					rank = row.rank,
+					account = row.player_account,
+					nickname = row.player_name,
+					country = row.player_country,
+					time = row.time_ms,
+					date = row.recorded_at
+				}
+			end
 		end
-
 	end
 
 	if not map then return end
 
 	for i, player in ipairs(players) do
-	
 		local account = getElementData(player, "account")
-		
-		if account then
-		
-			local personalBest = Leaderboard.personalBestCache[arenaElement] and Leaderboard.personalBestCache[arenaElement][account] or false
-			triggerClientEvent(player, "onClientTopTimesUpdate", player, Leaderboard.cache[arenaElement], personalBest)
-		
-		end
-		
+		local personalBest = Leaderboard.personalBestCache[arenaElement] and Leaderboard.personalBestCache[arenaElement][account] or false
+		triggerClientEvent(player, "onClientTopTimesUpdate", player, Leaderboard.cache[arenaElement], personalBest)
 	end
-	
 end
 
-
 function Leaderboard.fetchLeaderboard(map)
-
 	Leaderboard.oldCache[source] = nil
 	Leaderboard.cache[source] = nil
 	Leaderboard.personalBestCache[source] = nil
@@ -320,37 +275,24 @@ function Leaderboard.fetchLeaderboard(map)
 	
 	local query = dbPrepareString(connection, Leaderboard.selectQuery, map.resource, Leaderboard.ROWS_TO_SHOW)
 	dbQuery(Leaderboard.callback, {source, true}, connection, query)
-	
 end
 addEvent("onMapChange", true)
 addEventHandler("onMapChange", root, Leaderboard.fetchLeaderboard)
 
-
 function Leaderboard.clientRequest()
-
 	local arenaElement = getElementParent(source)
 	local map = getElementData(arenaElement, "map")
 
 	if not getElementData(arenaElement, "toptimes") or not Leaderboard.cache[arenaElement] or not map then return end
 
 	local account = getElementData(source, "account")
-	local personalBest = false
-	
-	if account then
-	
-		personalBest = Leaderboard.personalBestCache[arenaElement] and Leaderboard.personalBestCache[arenaElement][account] or false
-		
-	end
-
-	triggerClientEvent(arenaElement, "onClientTopTimesUpdate", arenaElement, Leaderboard.cache[arenaElement], personalBest)
-	
+	local personalBest = Leaderboard.personalBestCache[arenaElement] and Leaderboard.personalBestCache[arenaElement][account] or false
+	triggerClientEvent(source, "onClientTopTimesUpdate", source, Leaderboard.cache[arenaElement], personalBest)
 end
 addEvent("onPlayerRequestTopTimes", true)
 addEventHandler("onPlayerRequestTopTimes", root, Leaderboard.clientRequest)
 
-
 function Leaderboard.deleteRecord(p, c, indexToRemove)
-
 	local arenaElement = getElementParent(p)
 	local map = getElementData(arenaElement, "map")
 	indexToRemove = tonumber(indexToRemove)
@@ -370,30 +312,23 @@ function Leaderboard.deleteRecord(p, c, indexToRemove)
 	dbQuery(Leaderboard.callback, {arenaElement}, connection, query)
 	
 	exports["CCS"]:export_outputArenaChat(arenaElement, "#FF0000* #FFFFFF"..getPlayerName(p):gsub("#%x%x%x%x%x%x", "").." #ff5050removed top #FFFFFF#"..indexToRemove)
-	
 end
 addCommandHandler("deletetime", Leaderboard.deleteRecord)
 
-
 function Leaderboard.deathmatchFinish(type, model)
-
 	if not getPedOccupiedVehicle(source) or type ~= "vehiclechange" then return end
 
 	if model == 425 then
 		local time = exports["CCS"]:export_getTimePassed(getElementParent(source))
 		Leaderboard.addRecord(source, time)
 	end
-	
 end
 addEvent("onPlayerDerbyPickupHit", true)
 addEventHandler("onPlayerDerbyPickupHit", root, Leaderboard.deathmatchFinish)
 
-
 function Leaderboard.raceFinish()
-
 	local time = exports["CCS"]:export_getTimePassed(getElementParent(source))
 	Leaderboard.addRecord(source, time)
-	
 end
 addEvent("onFinishRace", true)
 addEventHandler("onFinishRace", root, Leaderboard.raceFinish)
