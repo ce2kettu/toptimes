dec6d9a71f5869fb30fe6abab56918fcaadc98f3
Teemu Ketonen <teemu.ket@gmail.com>
2019-04-13 16:11:58 +0300

    Update leaderboard_c.lua

--- leaderboard_c.lua
+++ leaderboard_c.lua
@@ -1,47 +1,66 @@
-local screenX, screenY = guiGetScreenSize()
-local ratioX, ratioY = (screenX / 800), (screenY / 600)
-local startX = 800 * ratioX
-local startY = 200 * ratioY
-local endX = nil
-local endY = startY
-local positionX = startX
-local positionY = startY
-local leaderboardWidth = 0
-local toggleDelay = nil
-local leaderboard = {}
-local isAnimating = false
-local animationStartTime = nil
-local animationEndTime = nil
-local tableColumns = {}
-local isToggled = false
-local isClientReady = false
-local personalRecord = false
-local isAlreadyShown = false
-local TOGGLE_KEY = "F5"
-local ANIMATION_NAME = "InOutQuad"
-local LEADERBOARD_OFFSET = 20 * ratioY
-local TOGGLE_DURATION = 5000
-local ROWS_TO_SHOW = 8
-local ROW_FONT_SIZE = 1
-local ROW_FONT_COLOR = tocolor(200, 200, 200, 255)
-local ROW_BACKGROUND_COLOR = tocolor(22, 28, 32, 225)
-local ROW_PADDING_START = 0
-local ROW_PADDING_END = 7.5 * ratioX
-local ROW_PADDING_Y = 3 * ratioY
-local ROW_FONT = dxCreateFont("fonts/Inter-UI-Regular.ttf", math.floor(8 * ratioY))
-local ROW_SIZE = dxGetFontHeight(ROW_FONT_SIZE, ROW_FONT) + ROW_PADDING_Y
-local COLUMN_HEIGHT_MULTIPLIER = 1.15
-local COLUMN_FONT_SIZE = 1
-local COLUMN_FONT = dxCreateFont("fonts/Inter-UI-Medium.ttf", math.floor(7 * ratioY))
-local COLUMN_BACKGROUND_COLOR = tocolor(14, 23, 26, 240)
-local COLUMN_FONT_COLOR = tocolor(255, 255, 255, 255)
-local BORDER_SIZE = 3
-local BORDER_LEFT_COLOR_HIGHLIGHT = tocolor(64, 196, 255, 255)
-local HIGHLIGHT_IMAGE_PATH = "images/gradient_highlight.png"
+screenX, screenY = guiGetScreenSize()
+ratioX, ratioY = (screenX / 800), (screenY / 600)
+startX = 800 * ratioX
+startY = 200 * ratioY
+endX = nil
+endY = startY
+positionX = startX
+positionY = startY
+leaderboardWidth = 0
+toggleDelay = nil
+leaderboard = {}
+isAnimating = false
+animationStartTime = nil
+animationEndTime = nil
+tableColumns = {}
+isToggled = false
+isClientReady = false
+personalRecord = false
+isAlreadyShown = false
 
-function main()
+TOGGLE_KEY = "F5"
+ANIMATION_NAME = "InOutQuad"
+LEADERBOARD_OFFSET = 20 * ratioY
+TOGGLE_DURATION = 5000
+ROWS_TO_SHOW = 8
+ROW_FONT_SIZE = 1
+ROW_FONT_COLOR = tocolor(200, 200, 200, 255)
+ROW_BACKGROUND_COLOR = tocolor(22, 28, 32, 225)
+ROW_PADDING_START = 0
+ROW_PADDING_END = 7.5 * ratioX
+ROW_PADDING_Y = 3 * ratioY
+ROW_FONT = dxCreateFont("fonts/Inter-UI-Regular.ttf", math.floor(8 * ratioY))
+ROW_SIZE = dxGetFontHeight(ROW_FONT_SIZE, ROW_FONT) + ROW_PADDING_Y
+COLUMN_HEIGHT_MULTIPLIER = 1.15
+COLUMN_FONT_SIZE = 1
+COLUMN_FONT = dxCreateFont("fonts/Inter-UI-Medium.ttf", math.floor(7 * ratioY))
+COLUMN_BACKGROUND_COLOR = tocolor(14, 23, 26, 240)
+COLUMN_FONT_COLOR = tocolor(255, 255, 255, 255)
+BORDER_SIZE = 3
+BORDER_LEFT_COLOR_HIGHLIGHT = tocolor(64, 196, 255, 255)
+HIGHLIGHT_IMAGE_PATH = "images/gradient_highlight.png"
 
-	addColumn({
+Leaderboard = {}
+Leaderboard.flags = {
+	"AD", "AE", "AF", "AG", "AI", "AL", "AM", "AN", "AO", "AR", "AS", "AT", "AU", "AW", "AX", "AZ",
+	"BA", "BB", "BD", "BE", "BF", "BG", "BH", "BI", "BJ", "BL", "BM", "BN", "BO", "BR", "BS", "BT",
+	"BW", "BY", "BZ", "CA", "CD", "CF", "CG", "CH", "CI", "CK", "CL", "CM", "CN", "CO", "CR", "CU",
+	"CV", "CW", "CY", "CZ", "DE", "DJ", "DK", "DM", "DO", "DZ", "EC", "EE", "EG", "EH", "ER", "ES",
+	"ET", "EU", "FI", "FJ", "FK", "FM", "FO", "FR", "GA", "GB", "GD", "GE", "GG", "GH", "GI", "GL",
+	"GM", "GN", "GQ", "GR", "GS", "GT", "GU", "GW", "GY", "HK", "HN", "HR", "HT", "HU", "IC", "ID",
+	"IE", "IL", "IM", "IN", "IQ", "IR", "IS", "IT", "JE", "JM", "JO", "JP", "KE", "KG", "KH", "KI",
+	"KM", "KN", "KP", "KR", "KW", "KY", "KZ", "LA", "LB", "LC", "LI", "LK", "LR", "LS", "LT", "LU",
+	"LV", "LY", "MA", "MC", "MD", "ME", "MF", "MG", "MH", "MK", "ML", "MM", "MN", "MO", "MP", "MQ",
+	"MR", "MS", "MT", "MU", "MV", "MW", "MX", "MY", "MZ", "NA", "NC", "NE", "NF", "NG", "NI", "NL",
+	"NO", "NP", "NR", "NU", "NZ", "OM", "PA", "PE", "PF", "PG", "PH", "PK", "PL", "PN", "PR", "PS",
+	"PT", "PW", "PY", "QA", "RO", "RS", "RU", "RW", "SA", "SB", "SC", "SD", "SE", "SG", "SH", "SI",
+	"SK", "SL", "SM", "SN", "SO", "SR", "SS", "ST", "SV", "SY", "SZ", "TC", "TD", "TF", "TG", "TH",
+	"TJ", "TK", "TL", "TM", "TN", "TO", "TR", "TT", "TV", "TW", "TZ", "UA", "UG", "US", "UY", "UZ",
+	"VA", "VC", "VE", "VG", "VI", "VN", "VU", "WF", "WS", "YE", "YT", "ZA", "ZM", "ZW", "XX"
+}
+
+function Leaderboard.main()
+	Leaderboard.addColumn({
 		title = "Rank", 
 		width = dxGetTextWidth("_XX_.", ROW_FONT_SIZE, ROW_FONT),
 		computed = function(row, index)
@@ -61,7 +80,7 @@ function main()
 		isImageCentered = true
 	})
 
-	addColumn({
+	Leaderboard.addColumn({
 		title = "Nickname", 
 		width = dxGetTextWidth("_XXXXXXXXXXXXXXX_", ROW_FONT_SIZE, ROW_FONT),
 		computed = function(row)
@@ -69,7 +88,7 @@ function main()
 		end
 	})
 
-	addColumn({
+	Leaderboard.addColumn({
 		title = "Time",
 		width = dxGetTextWidth("_00:00:000___", ROW_FONT_SIZE, ROW_FONT),
 		computed = function(row)
@@ -77,20 +96,19 @@ function main()
 		end
 	})
 
-	addColumn({
+	Leaderboard.addColumn({
 		title = "Date",
 		width = dxGetTextWidth("_XXXX.XX.XX__", ROW_FONT_SIZE, ROW_FONT),
 		computed = function(row)
-			return row and convertDate(row.date) or "—"
+			return row and Leaderboard.convertDate(row.date) or "—"
 		end
 	})
 
-	addColumn({
+	Leaderboard.addColumn({
 		title = "Country",
 		width = math.floor(12 * ratioY) + (tableColumns[1].width / 2),
 		computed = function(row)
-			local imageName = row and row.country or "_unknown"
-			return "images/flags/"..imageName..".png"
+			return row and row.country and Leaderboard.flags[row.country] or "XX"
 		end,
 		alignX = nil,
 		alignY = nil,
@@ -112,12 +130,14 @@ function main()
 
 	leaderboardWidth = leaderboardWidth
 	endX = startX - leaderboardWidth - LEADERBOARD_OFFSET
-	
+  
+	for i, flag in ipairs(Leaderboard.flags) do
+		Leaderboard.flags[flag] = i-1
+	end	
 end
-addEventHandler("onClientResourceStart", resourceRoot, main)
+addEventHandler("onClientResourceStart", resourceRoot, Leaderboard.main)
 
-function render()
-
+function Leaderboard.render()
 	local arena = getElementParent(localPlayer)
 	local map = getElementData(arena, "map")
 
@@ -162,13 +182,15 @@ function render()
 		currentX = currentX + ROW_PADDING_START 
 
 		for j, column in pairs(tableColumns) do
-			local value = column.computed(leaderboard[i], i)			
-
+			local value = column.computed(leaderboard[i], i)
 			if column.isValueImage then
+				local flagIndex = Leaderboard.flags[value]
+				local flagRow = flagIndex % 16
+				local flagColumn = math.floor(flagIndex / 16)
 				local imageSize = math.floor(column.rowImageSize * ratioY)
-				dxDrawImage(currentX, currentY + (ROW_SIZE / 2) - (imageSize / 2), imageSize, imageSize, value)
+				dxDrawImageSection(currentX, currentY + (ROW_SIZE / 2) - (imageSize / 2), imageSize, imageSize, (48) * flagRow, (48) * flagColumn, 48, 48, ":CCS/img/flags.png")
 			else
-				value = textOverflow(value, ROW_FONT_SIZE, ROW_FONT, column.width, true)
+				value = Leaderboard.textOverflow(value, ROW_FONT_SIZE, ROW_FONT, column.width, true)
 
 				if column.isImage then
 					dxDrawText(value, currentX + (column.imageSize / 2), currentY, currentX + column.width, currentY + ROW_SIZE, ROW_FONT_COLOR, ROW_FONT_SIZE, ROW_FONT, column.rowAlignX, column.rowAlignY, false, false, false, true, false)
@@ -192,15 +214,18 @@ function render()
 			local value = column.computed(personalRecord, personalRecord.rank)
 
 			if column.isValueImage then
+				local flagIndex = Leaderboard.flags[value]
+				local flagRow = flagIndex % 16
+				local flagColumn = math.floor(flagIndex / 16)
 				local imageSize = math.floor(column.rowImageSize * ratioY)
-				dxDrawImage(currentX, currentY + (ROW_SIZE / 2) - (imageSize / 2), imageSize, imageSize, value)
+				dxDrawImageSection(currentX, currentY + (ROW_SIZE / 2) - (imageSize / 2), imageSize, imageSize, (48) * flagRow, (48) * flagColumn, 48, 48, ":CCS/img/flags.png")
 			else
 				if column.isImage then
 					local textWidth = dxGetTextWidth(value, ROW_FONT_SIZE, ROW_FONT)
-					local fontSize = textFit(value, ROW_FONT_SIZE, ROW_FONT, column.width, 10)
+					local fontSize = Leaderboard.textFit(value, ROW_FONT_SIZE, ROW_FONT, column.width, 10)
 					dxDrawText(value, currentX + (column.imageSize / 2), currentY, currentX + column.width, currentY + ROW_SIZE, ROW_FONT_COLOR, fontSize, ROW_FONT, column.rowAlignX, column.rowAlignY, false, false, false, true, false)
 				else
-					value = textOverflow(value, ROW_FONT_SIZE, ROW_FONT, column.width, true)
+					value = Leaderboard.textOverflow(value, ROW_FONT_SIZE, ROW_FONT, column.width, true)
 					dxDrawText(value, currentX, currentY, currentX + column.width, currentY + ROW_SIZE, ROW_FONT_COLOR, ROW_FONT_SIZE, ROW_FONT, column.rowAlignX, column.rowAlignY, false, false, false, true, false)
 				end
 			end
@@ -208,56 +233,44 @@ function render()
 			currentX = currentX + column.width
 		end
 	end
-	
 end
 
-function updateLeaderboard(list, personalBest, isInitialFetch)
-
+function Leaderboard.update(list, personalBest)
 	local arena = getElementParent(localPlayer)
 	local map = getElementData(arena, "map")
 	personalRecord = false
 
-	--[[ if isAlreadyShown and isInitialFetch then return end ]]
 	if not isClientReady or not getElementData(arena, "toptimes") or not map or not list then return end
-
---[[ 	if isInitialFetch then 
-		isAlreadyShown = true
-	else
-		isAlreadyShown = false
-	end ]]
 
 	if personalBest then
 		personalRecord = personalBest
 	end
 
-	removeEventHandler("onClientRender", root, animationOut)
-	removeEventHandler("onClientRender", root, animationIn)
+	removeEventHandler("onClientRender", root, Leaderboard.animationOut)
+	removeEventHandler("onClientRender", root, Leaderboard.animationIn)
 	removeEventHandler("onClientRender", root, render)
 	leaderboard = list
 	isAnimating = false
 	isToggled = false
-	setTimer(function() toggle() end, 1000, 1)
-	
+	setTimer(function() Leaderboard.toggle() end, 1000, 1)	
 end
 addEvent("onClientTopTimesUpdate", true)
-addEventHandler("onClientTopTimesUpdate", root, updateLeaderboard)
+addEventHandler("onClientTopTimesUpdate", root, Leaderboard.update)
 
-
-addEvent("onClientPlayerSpawn", true)
-addEventHandler("onClientPlayerSpawn", root, function()
+addEvent("onClientPlayerReady", true)
+addEventHandler("onClientPlayerReady", root, function()
 	if isClientReady then return end
+
 	isClientReady = true
 	triggerServerEvent("onPlayerRequestTopTimes", localPlayer)
 end)
-
 
 addEvent("onClientMapChange", true)
 addEventHandler("onClientMapChange", root, function()
 	isClientReady = false
 end)
 
-
-function toggle()
+function Leaderboard.toggle()
 	local arena = getElementParent(localPlayer)
 
 	if not getElementData(arena, "toptimes") or isAnimating then return end
@@ -269,19 +282,18 @@ function toggle()
 	if isToggled then
 		isAnimating = true
 		isToggled = false
-		addEventHandler("onClientRender", root, animationOut)
+		addEventHandler("onClientRender", root, Leaderboard.animationOut)
 	else
 		isAnimating = true
 		isToggled = true
-		addEventHandler("onClientRender", root, animationIn)
+		addEventHandler("onClientRender", root, Leaderboard.animationIn)
 		addEventHandler("onClientRender", root, render)
 		toggleDelay = setTimer(toggle, TOGGLE_DURATION, 1)
 	end
 end
-bindKey(TOGGLE_KEY, "down", toggle)
+bindKey(TOGGLE_KEY, "down", Leaderboard.toggle)
 
-
-function animationIn()
+function Leaderboard.animationIn()
 	local now = getTickCount()
 	local elapsedTime = now - animationStartTime
 	local duration = animationEndTime - animationStartTime
@@ -290,13 +302,12 @@ function animationIn()
 	positionX, positionY = interpolateBetween(startX, startY, 0, endX, endY, 0, progress, ANIMATION_NAME)
 
 	if progress >= 1 then
-		removeEventHandler("onClientRender", root, animationIn)
+		removeEventHandler("onClientRender", root, Leaderboard.animationIn)
 		isAnimating = false
 	end
 end
 
-
-function animationOut()
+function Leaderboard.animationOut()
 	local now = getTickCount()
 	local elapsedTime = now - animationStartTime
 	local duration = animationEndTime - animationStartTime
@@ -305,27 +316,25 @@ function animationOut()
 	positionX, positionY = interpolateBetween(endX, endY, 0, startX, startY, 0, progress, ANIMATION_NAME)
 
 	if progress >= 1 then
-		removeEventHandler("onClientRender", root, animationOut)
-		removeEventHandler("onClientRender", root, render)
+		removeEventHandler("onClientRender", root, Leaderboard.animationOut)
+		removeEventHandler("onClientRender", root, Leaderboard.render)
 		isAnimating = false
 	end
 end
 
-
-function reset()
+function Leaderboard.reset()
 	if isTimer(toggleDelay) then killTimer(toggleDelay) end
 
-	removeEventHandler("onClientRender", root, render)
-	removeEventHandler("onClientRender", root, animationIn)
-	removeEventHandler("onClientRender", root, animationOut)
+	removeEventHandler("onClientRender", root, Leaderboard.render)
+	removeEventHandler("onClientRender", root, Leaderboard.animationIn)
+	removeEventHandler("onClientRender", root, Leaderboard.animationOut)
 	isToggled = false
 	isAnimating = false
 end
 addEvent("onClientArenaReset", true)
-addEventHandler("onClientArenaReset", root, reset)
+addEventHandler("onClientArenaReset", root, Leaderboard.reset)
 
-
-function addColumn(column)
+function Leaderboard.addColumn(column)
 	column.alignX = column.alignX or "left"
 	column.alignY = column.alignY or "center"
 	column.rowAlignX = column.rowAlignX or column.alignX
@@ -336,12 +345,10 @@ function addColumn(column)
 	column.isValueImage = column.isValueImage or false
 	column.rowImageSize = column.rowImageSize or column.imageSize
 	column.isImageCentered = column.isImageCentered or false
-
 	table.insert(tableColumns, column)
 end
 
-
-function textFit(text, size, font, width, padding)
+function Leaderboard.textFit(text, size, font, width, padding)
 	local fontSize = size
 	padding = padding or 10
 	width = width - padding
@@ -353,8 +360,7 @@ function textFit(text, size, font, width, padding)
 	return fontSize
 end
 
-
-function textOverflow(text, size, font, width, ellipsis)
+function Leaderboard.textOverflow(text, size, font, width, ellipsis)
 	local ellipsis = ellipsis or false
 
 	while dxGetTextWidth(text, size, font, true) > width do
@@ -368,9 +374,9 @@ function textOverflow(text, size, font, width, ellipsis)
 	return text
 end
 
-
-function convertDate(date)
+function Leaderboard.convertDate(date)
 	if not date then return false end
+
     local d, m, y, h, i, s = string.match(date, "(%d+)-(%d+)-(%d+) (%d+):(%d+):(%d+)")
     return string.format("%s.%s.%s", y, m, d) or false
 end
